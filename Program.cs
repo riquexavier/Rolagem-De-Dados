e3faf8d7ef9e744410ecb9c780df0971460395cb
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-04 18:22:48 -0300

    RerollDeDados

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//int produz mesmo efeito que o string, poém apenas para números
+//introduzir o dado no programa
+using System.Security.Cryptography;
+
+Console.Clear();
+
+Console.WriteLine("Este programa irá rolar um dado e parar em um número 'aleatório'.");
+
+Console.WriteLine();
+
+Console.Write("Quantas faces você deseja que tenha o dado? ");
+
+//int32 para limitar a quantia de números suportados, dados não é grande então usa-se int32bits. Caso necessário, int64bits
+int faces = Convert.ToInt32(Console.Read());
+
+//rolagem do dado
+int rolagem = RandomNumberGenerator.GetInt32(1, faces);
+
+Console.WriteLine();
+
+Console.WriteLine($"A rolagem do dado resultou em {rolagem}");
